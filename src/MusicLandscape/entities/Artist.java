957142a14e81c685fa0adb14458d143bcdc694ba
KatharinaGitStar <168305372+KatharinaGitStar@users.noreply.github.com>
2024-05-19 13:19:54 +0200

    intital versions of java class files
    Initial versions of java class files Artist, Event and Track. Artist tested already, with 0 failures.

---
+++ src/MusicLandscape/entities/Artist.java
@@ -0,0 +1,30 @@
+package MusicLandscape.entities;
+
+public class Artist {
+    private String name;
+
+    // Constructor with a default name unknown
+    public Artist() {
+        this.name = "unknown";
+    }
+
+    public Artist(Artist a) {
+        this.name = a.name;
+    }
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            this.name = "unknown";
+        } else {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/MusicLandscape/entities/Event.java
@@ -0,0 +1,54 @@
+package MusicLandscape.entities;
+
+public class Event {
+    private Artist artist;
+    private int attendees;
+    private Date date;
+    private String description;
+    private Venue venue;
+
+    public Event(){
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+    public void setArtist(Artist artist) {
+        if(artist != null) {
+            this.artist = artist;
+        }
+    }
+    public Venue getVenue(){
+        return venue;
+    }
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+    public Date getDate() {
+        return new Date(date);
+    }
+    public void setDate(Date date) {
+        if(date != null) {
+            this.date = new Date(date);
+        }
+        else{
+            this.date = null;
+        }
+    }
+    public int getAttendees() {
+        return attendees;
+    }
+    public void setAttendees(int attendees) {
+        if(attendees >= 0) {
+            this.attendees = attendees;
+        }
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        if(description != null){
+            this.description = description;
+        }
+    }
+}
